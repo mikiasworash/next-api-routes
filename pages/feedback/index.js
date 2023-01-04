ef191f0cc421522210514b2b61583617d23934b5
mikias-worash <mikias.worash.a@gmail.com>
2023-01-04 12:08:08 +0300

    Added dynamic feedback page

---
+++ pages/feedback/index.js
@@ -0,0 +1,43 @@
+import { Fragment, useState } from 'react'
+import fs from 'fs'
+import path from 'path'
+
+export default function feedbackPage(props) {
+  const [feedbackDetail, setFeedbackDetail] = useState()
+
+  function loadFeedbackDetail(id) {
+    fetch(`/api/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackDetail(data.feedback)
+      })
+  }
+
+  return (
+    <Fragment>
+      {feedbackDetail && <p>{feedbackDetail.email}</p>}
+      <ul>
+        {props.feedbackItems.map((item) => (
+          <li key={item.id}>
+            {item.feedback}{' '}
+            <button onClick={loadFeedbackDetail.bind(null, item.id)}>
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>{' '}
+    </Fragment>
+  )
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'feedback.json')
+  const fileData = fs.readFileSync(filePath)
+  const data = JSON.parse(fileData)
+
+  return {
+    props: {
+      feedbackItems: data,
+    },
+  }
+}
